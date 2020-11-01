5789e47be5e53951d51c93939b154aa54004ba57
daniel.monroy <daniel.monroym@udea.edu.co>
2020-11-01 17:44:15 -0500

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./index/index.module').then( m => m.IndexPageModule)
   }
 ];
 @NgModule({
